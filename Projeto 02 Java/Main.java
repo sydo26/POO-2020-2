228ced5ff462e45f5301aec3daeec505abf842ed
Vinícius Roque <sydoafk@gmail.com>
2020-12-12 00:11:44 -0300

    Removi projeto 02 Java e alterei o nome do projeto 03 java para projeto 02

--- Projeto 02 Java/Main.java
+++ Projeto 02 Java/Main.java
@@ -1,31 +1,35 @@
 import java.util.Scanner;
+
+import models.Enemy;
+import models.Player;
+
 public class Main {
-    public static void main(String[] args) throws Exception{
-        Scanner input = new Scanner(System.in);
-        System.out.println("Digite o combustível atual do avião e a distância que ele percorrerá para iniciarmos a simulação! Formato: {combustivel} {distancia}");
-        Aviao obj = new Aviao(input.nextDouble(), input.nextDouble());
-        Thread.sleep(1000);
-        System.out.println("Avião preparando para decolar!");
-        Thread.sleep(1000);
-        obj.voar();
-        System.out.println("Avião decolou!!");
-        double distancia = obj.getDistancia();
-        for(int i = 0; i < (int) distancia; i++) {
-            Thread.sleep(100);
-            if(obj.getCombustivel() <= 0) break;
-            if(obj.getDistancia() <= 0) break;
-            obj.setDistancia((obj.getDistancia() - 10) < 0 ? 0 : obj.getDistancia() - 10);
-            obj.setCombustivel(obj.getCombustivel() - 1);
-            System.out.println(obj);
-        }
+    public static Player player;
+    public static Enemy enemy;
+    
+    public static void clearConsole() {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();     
+    }
 
-        if(obj.getCombustivel() >= 0) obj.pousar();
-
-        if(obj.isVoando()) {
-            System.out.println("Avião caiu...!");
-        } else {
-            System.out.println("Avião chegou ao seu destino, está pousando...");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            clearConsole();
+            System.out.print("Escolha o nome de seu personagem: ");
+            String name = scanner.nextLine();
+            if(name != "") {
+                player = new Player(name);
+                break;
+            }
         }
+        enemy = new Enemy("MONSTRO");
+
+        System.out.println(player);
+        System.out.println(enemy);
+        System.out.println("");
+        player.startAttack(enemy);
+        enemy.startAttack(player);
 
     }
 }
